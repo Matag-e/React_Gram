8dc100c33319336ffa3224b83fc9c0fbee275f36
Matag-E <mateussantos212@gmail.com>
2024-09-04 14:57:22 -0300

    subindo

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-<<<<<<< HEAD
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
 
@@ -13,27 +12,6 @@ const conn = async () => {
     console.error("Erro ao conectar ao banco de dados:", error);
   }
 };
-=======
-const dbUser = process.env.DB_USER
-const dbPassword = process.env.DB_PASSWORD
-
-const conn = async () => {
-    try{
-        const dbConn = await mongoose.connect(
-<<<<<<< HEAD
-            `mongodb+srv://${dbUser}:${dbPassword}@reactgram.cwiwhbg.mongodb.net/?retryWrites=true&w=majority&appName=ReactGram`
-=======
-            `mongodb+srv://${dbUser}:${dbPassword}@cluster0.2qbzvx6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
->>>>>>> d1fa4c6c6362080cff7be934a28a04091cd51171
-        )
-        console.log("Conectou ao banco!!")
-        return dbConn;
-    }catch(error){
-        console.log(error)
-    }
-}
-
->>>>>>> 529039b3f60c409868cc59045320dd7d29cc9d5c
 
 conn();
 

4a9162f7c7b566e525ae580917b2b5ba211764af
Matag-E <mateussantos212@gmail.com>
2024-09-04 14:56:02 -0300

    teste

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,17 @@
 const mongoose = require("mongoose");
-const dbUser = process.env.DB_USER
-const dbPassword = process.env.DB_PASS
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
 
 const conn = async () => {
-    try{
-        const dbConn = await mongoose.connect(
-            `mongodb+srv://${dbUser}:${dbPassword}@cluster0.2qbzvx6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-        )
-        console.log("Conectou ao banco!!")
-        return dbConn;
-    }catch(error){
-        console.log(error)
-    }
-}
-
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.tgfnn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    );
+    console.log("Conectou ao banco!!");
+  } catch (error) {
+    console.error("Erro ao conectar ao banco de dados:", error);
+  }
+};
 
 conn();
 

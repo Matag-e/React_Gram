1a66f9ccc1b4ccd374bec01bab4bc3557fb977e7
Matag-e <mateussantos212@gmail.com>
2026-01-10 14:05:36 -0300

    Make photo GET routes public

--- backend/routes/PhotoRoutes.js
+++ backend/routes/PhotoRoutes.js
@@ -34,10 +34,10 @@ router.post(
 );
 
 router.delete("/:id", authGuard, deletePhoto);
-router.get("/", authGuard, getAllPhotos);
-router.get("/user/:id", authGuard, getUserPhotos);
-router.get("/search", authGuard, searchPhotos);
-router.get("/:id", authGuard, getPhotoById);
+router.get("/", getAllPhotos);
+router.get("/user/:id", getUserPhotos);
+router.get("/search", searchPhotos);
+router.get("/:id", getPhotoById);
 router.put("/:id", authGuard, photoUpdateValidation(), validate, updatePhoto);
 router.put("/like/:id", authGuard, likePhoto);
 router.put(

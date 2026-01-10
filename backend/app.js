eb536dc606134c414f32fdb98860f4441682b179
Matag-e <mateussantos212@gmail.com>
2026-01-10 14:00:23 -0300

    Allow Vercel domain in CORS

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Solve CORS
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cors({ 
+  credentials: true, 
+  origin: ["http://localhost:5173", "https://react-gram-opal.vercel.app"] 
+}));
 
 // Upload directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
